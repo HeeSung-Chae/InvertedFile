8ede4e85a085aa7a3d246adeaddacfdc1a294692
chaeheesung <cjdrn104ehd@naver.com>
2018-07-09 17:36:28 +0900

    1차 출력 완료(불필요 삭제 전)

--- src/com/company/Another.java
+++ src/com/company/Another.java
@@ -7,39 +7,65 @@ public class Another
 {
     public static void main(String[ ] args) throws IOException
     {
-        TreeMap<String, Integer> frequencyData = new TreeMap<String, Integer>( );
+//        TreeMap<String, String> frequencyData = new TreeMap<>( );
+//        readWordFile(frequencyData);
+//        printAllCounts(frequencyData);
+
+        TreeMap<String, TreeMap<Integer, Integer>> frequencyData = new TreeMap<>( );
 
         readWordFile(frequencyData);
         printAllCounts(frequencyData);
     }
 
 
-    public static int getCount(String word, TreeMap<String, Integer> frequencyData)
+    public static TreeMap<Integer, Integer> getCount(String word, int docNum, TreeMap<String, TreeMap<Integer, Integer>> frequencyData)
     {
-        if (frequencyData.containsKey(word))
-        {  // The word has occurred before, so get its count from the map
-            return frequencyData.get(word); // Auto-unboxed
+//        frequencyData.get(word)
+        if(frequencyData.containsKey(word))
+        {
+            if(frequencyData.get(word).containsKey(docNum))
+            {
+                frequencyData.get(word).put(docNum, frequencyData.get(word).get(docNum) + 1);
+                return frequencyData.get(word);
+            }
+            else
+            {
+                frequencyData.get(word).put(docNum, 1);
+                return frequencyData.get(word);
+            }
         }
         else
-        {  // No occurrences of this word
-            return 0;
+        {
+            frequencyData.put(word, new TreeMap<Integer, Integer>(){
+                {
+                    put(docNum, 1);
+                }
+            });
+            return frequencyData.get(word);
         }
     }
 
-
-    public static void printAllCounts(TreeMap<String, Integer> frequencyData)
+    public static void printAllCounts(TreeMap<String, TreeMap<Integer, Integer>> frequencyData)
     {
         frequencyData.remove("");
 
         for(String word : frequencyData.keySet( ))
         {
-            System.out.println(word + " " + frequencyData.get(word));
+            System.out.print(word);
+            Iterator iterator = frequencyData.get(word).keySet().iterator();
+            Object object;
+            while(iterator.hasNext())
+            {
+                object = iterator.next();
+                System.out.print(" " + object + " " + frequencyData.get(word).get(object));
+            }
+            System.out.println();
         }
         System.out.println(frequencyData.size());
     }
 
 
-    public static void readWordFile(TreeMap<String, Integer> frequencyData) throws IOException
+    public static void readWordFile(TreeMap<String, TreeMap<Integer, Integer>> frequencyData) throws IOException
     {
         File path_input = new File(input);
         FileInputStream iStream = new FileInputStream(path_input.getAbsolutePath());
@@ -47,13 +73,14 @@ public class Another
 
         Integer count;
 
-        String strLine;     // Input Sentence
+        // Input Sentence
+        String strLine;
         while((strLine = iReader.readLine()) != null)
         {
             strLine = strLine.toLowerCase();
 
             // Document Number, Sentence
-            String docNum = strLine.split(" ")[0];
+            int docNum = Integer.parseInt(strLine.split(" ")[0]);
             String sentence = strLine.replace(docNum + " ", "");
 
             // 문자 처리
@@ -63,12 +90,88 @@ public class Another
             // Get the current count of this word, add one, and then store the new count
             for(String s : reSent.split(" "))
             {
-                count = getCount(s, frequencyData) + 1;
-                frequencyData.put(s, count);
+                TreeMap<Integer, Integer> temp = getCount(s, docNum, frequencyData);
+                frequencyData.put(s, temp);
             }
         }
     }
 
+
+    public static void readWordFile3(TreeMap<String, String> frequencyData) throws IOException
+    {
+        File path_input = new File(input);
+        FileInputStream iStream = new FileInputStream(path_input.getAbsolutePath());
+        BufferedReader iReader = new BufferedReader(new InputStreamReader(iStream));
+
+        Integer count;
+
+        // Input Sentence
+        String strLine;
+        while((strLine = iReader.readLine()) != null)
+        {
+            strLine = strLine.toLowerCase();
+
+            // Document Number, Sentence
+            int docNum = Integer.parseInt(strLine.split(" ")[0]);
+            String sentence = strLine.replace(docNum + " ", "");
+
+            // 문자 처리
+            String regex = "\\W";
+            String reSent = sentence.replaceAll(regex, " ");
+
+            // Get the current count of this word, add one, and then store the new count
+            for(String s : reSent.split(" "))
+            {
+                frequencyData.put(s, getCount3(s, docNum, frequencyData));
+            }
+        }
+    }
+
+    public static String getCount3(String word, int docNum, TreeMap<String, String> frequencyData)
+    {
+        if(frequencyData.containsKey(word))
+        {
+            int count = 1;
+            Boolean change = false;
+            String temp = "";
+            for(String s : frequencyData.get(word).split(" "))
+            {
+                if(count%2 != 0)
+                {
+                    if(s.equals(docNum))
+                    {
+                        change = true;
+                        temp += docNum;
+                    }
+                }
+                count++;
+
+                if(change == true && count%2 == 0)
+                {
+                    s = String.valueOf(Integer.parseInt(s) + 1);
+                    temp += s;
+                    change = false;
+                }
+            }
+            return temp;
+        }
+        else
+        {
+            frequencyData.put(word, docNum + " 1");
+            return frequencyData.get(word);
+        }
+    }
+
+    public static void printAllCounts3(TreeMap<String, String> frequencyData)
+    {
+        frequencyData.remove("");
+
+        for(String word : frequencyData.keySet( ))
+        {
+            System.out.println(word + " / " + frequencyData.get(word));
+        }
+        System.out.println(frequencyData.size());
+    }
 
     // Array of documents
     static String input = "src/com/company/data/input.small";
