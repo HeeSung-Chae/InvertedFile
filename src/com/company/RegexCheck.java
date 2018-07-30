c061e57dad74df259e88a1552e192feb19c8a1fc
chaeheesung <cjdrn104ehd@naver.com>
2018-07-30 09:38:24 +0900

    저장

---
+++ src/com/company/RegexCheck.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.io.*;
+import java.util.regex.Pattern;
+
+public class RegexCheck {
+    static String input = "I:\\아이브릭스 프로그래밍 테스트\\제출 파일\\재 제출 파일\\output.big";
+
+    public static void main(String[] args) throws IOException {
+        String a = "1";
+        int aa = 1;
+        if(a.equals(String.valueOf(aa)))
+            System.out.println("asdf");
+//        File path = new File(input);
+//        FileInputStream inputStream = new FileInputStream(path);
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+//
+//        String strLine;
+//        while((strLine = bufferedReader.readLine()) != null)
+//        {
+//            String word = strLine.split(" ")[0];
+//            boolean flag = Pattern.matches("^[a-zA-Z0-9]*$", word);
+//            if(flag == flag)
+//                System.out.println(word);
+//        }
+    }
+}
